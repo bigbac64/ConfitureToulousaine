fd562954aec0802d76978758133fdc17c7a20f8a
bigbac64 <bigbac64@hotmail.fr>
2025-01-26 16:19:33 +0100

    fix win

--- Assets/ClassIngredient/TheatreRecolte.cs
+++ Assets/ClassIngredient/TheatreRecolte.cs
@@ -3,23 +3,25 @@ using UnityEngine;
 
 public class TheatreRecolte : MonoBehaviour, IRecolte
 {
-    public float scopeTesson = 0.2f;
+    public float scopeTesson = 0.5f;
 
     public float diffTesson = 0.3f;
 
-    public float luckySlime = 0.75f;
+    public float luckySlime = 0.1f;
 
     public Dictionary<string, int> recolte(float x)
     {
         float found = x * diffTesson;
         Dictionary<string, int> dict = new Dictionary<string, int>
         {
-            { "tesson", Random.Range(1 + Mathf.FloorToInt(found * (1 - scopeTesson)), 1 + Mathf.FloorToInt(found * (1 + scopeTesson))) },
+            { "canette", Random.Range(1 + Mathf.FloorToInt(found * (1 - scopeTesson)), 1 + Mathf.FloorToInt(found * (1 + scopeTesson))) },
+            { "pilier", Random.Range(1 + Mathf.FloorToInt(found * (1 - scopeTesson)), 1 + Mathf.FloorToInt(found * (1 + scopeTesson))) },
+            { "mousse", Random.Range(1 + Mathf.FloorToInt(found * (1 - scopeTesson)), 1 + Mathf.FloorToInt(found * (1 + scopeTesson))) },
         };
 
         if (Random.value > luckySlime)
         {
-            dict.Add("slime", Random.Range(3, 5));
+            dict.Add("slime", Random.Range(3, 10));
         }
 
         return dict;
